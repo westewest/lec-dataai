0772d43a728eed089171feccff08399d723faa6d
Hiroaki Nishi <west@keio.jp>
2022-09-27 13:51:46 +0900

    update options

--- Tools/colablocal.sh
+++ Tools/colablocal.sh
@@ -1,9 +1,21 @@
-jupyter notebook \
+#jupyter-notebook \
+#	--no-browser \
+#	--allow-root \
+#	--ServerApp.ip=0.0.0.0 \
+#	--ServerApp.port=8888 \
+#	--NotebookApp.allow_origin='https://colab.research.google.com' \
+#	--ServerApp.port_retries=0 \
+#	--ServerApp.token='' \
+#	--ServerApp.disable_check_xsrf=True \
+#	--ServerApp.password=''
+
+jupyter-notebook \
 	--no-browser \
 	--allow-root \
 	--ServerApp.ip=0.0.0.0 \
-	--ServerApp.port=24088 \
-	--ServerApp.allow_origin='https://colab.research.google.com' \
-	--ServerApp.port_retries=0 \
-	--ServerApp.token='' \
-	--ServerApp.disable_check_xsrf=True
+	--ServerApp.port=8888 \
+	--NotebookApp.allow_origin='https://colab.research.google.com' \
+	--NotebookApp.port_retries=0 \
+	--NotebookApp.token='' \
+	--NotebookApp.disable_check_xsrf=True \
+	--NotebookApp.password=''
