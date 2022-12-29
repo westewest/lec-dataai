e4c17614f06147eee29e1326e60ee985327e5802
west <west@athenergy.com>
2022-12-29 20:43:54 +0900

    rollback

--- Tools/colablocal.sh
+++ Tools/colablocal.sh
@@ -1,21 +1,22 @@
-jupyter-notebook \
-	--no-browser \
-	--ServerApp.password=''
-	--ServerApp.allow_origin='https://colab.research.google.com' \
-	--ServerApp.port_retries=0 \
-	--ServerApp.token='' \
-	--ServerApp.disable_check_xsrf=True \
-	--ServerApp.password=''
-	--ServerApp.allow-root \
-	--ServerApp.ip=0.0.0.0 \
-	--ServerApp.port=8888 \
-
 #jupyter-notebook \
 #	--no-browser \
-#	--allow-root \
+#	--ServerApp.password=''
+#	--ServerApp.allow_origin='https://colab.research.google.com' \
+#	--ServerApp.port_retries=0 \
+#	--ServerApp.token='' \
+#	--NotebookApp.token='' \
+#	--ServerApp.disable_check_xsrf=True \
+#	--ServerApp.allow-root \
 #	--ServerApp.ip=0.0.0.0 \
 #	--ServerApp.port=8888 \
-#	--NotebookApp.allow_origin='https://colab.research.google.com' \
-#	--NotebookApp.port_retries=0 \
-#	--NotebookApp.token='' \
-##	--NotebookApp.disable_check_xsrf=True \
+
+jupyter-notebook \
+	--no-browser \
+	--allow-root \
+	--ServerApp.password='' \
+	--ServerApp.ip=0.0.0.0 \
+	--ServerApp.port=8888 \
+	--ServerApp.allow_origin='https://colab.research.google.com' \
+	--ServerApp.port_retries=0 \
+	--ServerApp.disable_check_xsrf=True \
+	--NotebookApp.token='' \
