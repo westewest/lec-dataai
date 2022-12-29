b4fb95f68add6d91c8a61c67319838d6c88c9c5a
west <west@athenergy.com>
2022-12-29 20:32:06 +0900

    update script to catch new version

--- Tools/colablocal.sh
+++ Tools/colablocal.sh
@@ -1,21 +1,21 @@
+jupyter-notebook \
+	--no-browser \
+	--ServerApp.password=''
+	--ServerApp.allow_origin='https://colab.research.google.com' \
+	--ServerApp.port_retries=0 \
+	--ServerApp.token='' \
+	--ServerApp.disable_check_xsrf=True \
+	--ServerApp.password=''
+	--ServerApp.allow-root \
+	--ServerApp.ip=0.0.0.0 \
+	--ServerApp.port=8888 \
+
 #jupyter-notebook \
 #	--no-browser \
 #	--allow-root \
 #	--ServerApp.ip=0.0.0.0 \
 #	--ServerApp.port=8888 \
 #	--NotebookApp.allow_origin='https://colab.research.google.com' \
-#	--ServerApp.port_retries=0 \
-#	--ServerApp.token='' \
-#	--ServerApp.disable_check_xsrf=True \
-#	--ServerApp.password=''
-
-jupyter-notebook \
-	--no-browser \
-	--allow-root \
-	--ServerApp.ip=0.0.0.0 \
-	--ServerApp.port=8888 \
-	--NotebookApp.allow_origin='https://colab.research.google.com' \
-	--NotebookApp.port_retries=0 \
-	--NotebookApp.token='' \
-	--NotebookApp.disable_check_xsrf=True \
-	--NotebookApp.password=''
+#	--NotebookApp.port_retries=0 \
+#	--NotebookApp.token='' \
+##	--NotebookApp.disable_check_xsrf=True \
